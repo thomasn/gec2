9f0244450897843dd62f1dccd336a73f8d07a7b8
Tim Dysinger <tim@dysinger.net>
2008-09-24 01:33:42 -1000

    corrected circular dependencies in dyndns

--- modules/ddclient/manifests/dyndns.pp
+++ modules/ddclient/manifests/dyndns.pp
@@ -1,7 +1,6 @@
 class ddclient::dyndns inherits ddclient {
   file { "/etc/ddclient/ddclient.conf":
     content => template("ddclient/dyndns.conf.erb"),
-    notify => Service["ddclient"],
-    require => Class["ddclient"]
+    notify => Service["ddclient"]
   }
 }
