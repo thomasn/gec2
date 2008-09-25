af40e60e70222fa46a69f74a7238fcb146cec212
Tim Dysinger <tim@dysinger.net>
2008-09-25 12:28:41 -1000

    added version to gem define

--- modules/ruby/manifests/init.pp
+++ modules/ruby/manifests/init.pp
@@ -6,5 +6,7 @@ class ruby {
   portage::keywords { "rubygems": category => "dev-ruby" }
   package { "rubygems": category => "dev-ruby", require => Package["ruby"] }
 
-  define gem { package { $name: provider => gem } }
+  define gem($version = installed) {
+    package { $name: provider => gem, ensure => $version }
+  }
 }
