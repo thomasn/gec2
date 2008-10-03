37a71631ac07287f44eab4cd1a7a943419f6d016
Tim Dysinger <tim@dysinger.net>
2008-10-02 22:22:10 -1000

    improved mysql and add database creation

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -1,5 +1,33 @@
-class mysql inherits base {
-  package { "mysql": category => "dev-db" }
-  exec { "/usr/bin/mysql_install_db": before => Service["mysql"] }
+class mysql {
+  package { "mysql":
+    category => "dev-db",
+    before => File["/etc/mysql/my.cnf"]
+  }
+  file { "/etc/mysql/my.cnf":
+    source => "puppet:///mysql/my.cnf",
+    before => Exec["mysql-init"]
+  }
+  exec { "mysql-init":
+    command => "/usr/bin/mysql_install_db",
+    creates => "/var/lib/mysql/mysql",
+    before => Service["mysql"]
+  }
   service { "mysql": ensure => running, enable => true }
+
+  define setup($db_user, $db_pass) {
+    exec { "mysql-create-db":
+      command => "/usr/bin/mysqladmin create $name",
+      unless => "/usr/bin/test -d /var/lib/mysql/$name",
+      notify => Exec["mysql-create-user"],
+      before => Exec["mysql-create-user"],
+      require => Service["mysql"]
+    }
+    exec { "mysql-create-user":
+      command => "/usr/bin/mysql -e \"\
+      GRANT ALL ON $name.* to '$db_user'@'10.%.%.%' IDENTIFIED BY '$db_pass'; \
+      GRANT ALL ON $name.* to '$db_user'@'localhost' IDENTIFIED BY '$db_pass'; \
+      FLUSH PRIVILEGES;\"",
+      refreshonly => true
+    }
+  }
 }
--- modules/mysql/manifests/ruby.pp
+++ modules/mysql/manifests/ruby.pp
@@ -1,4 +1,3 @@
 class mysql::ruby {
-  include ruby
   package { "mysql-ruby": category => "dev-ruby" }
 }
