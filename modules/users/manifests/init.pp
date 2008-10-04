5987555d6b090e751a0f3670a7215aca38235d12
Tim Dysinger <tim@dysinger.net>
2008-10-04 11:43:47 -1000

    added user module

---
+++ modules/users/manifests/init.pp
@@ -0,0 +1,31 @@
+class users {
+  define account($uid, $groups = []) {
+    user { $name:
+      uid => $uid,
+      gid => 100,
+      groups => $groups,
+      shell => '/bin/bash',
+      managehome => true,
+      ensure => present,
+      allowdupe => false,
+      before => [ File["/home/$name/.ssh"],
+                  File["/home/$name/.ssh/authorized_keys"] ],
+      notify => Exec["scramble-password-$name"]
+    }
+    exec { "scramble-password-$name":
+      command => "/usr/sbin/usermod -p
+      `dd if=/dev/urandom count=50 2>/dev/null | md5sum | cut -d ' ' -f1-1`
+      $name",
+      refreshonly => true
+    }
+    file { "/home/$name/.ssh":
+      ensure => directory, owner => $name, group => 'users', mode => 700
+    }
+    file { "/home/$name/.ssh/authorized_keys":
+      owner => $name,
+      group => 'users',
+      source => "puppet:///users/$name.pub",
+      mode => 600
+    }
+  }
+}
