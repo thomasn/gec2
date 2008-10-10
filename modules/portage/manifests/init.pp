49df845574ed31a9c7d902e0b9b97cd16fa4a9a5
Tim Dysinger <tim@dysinger.net>
2008-10-10 12:15:28 -1000

    formatting for 80 columns

--- modules/portage/manifests/init.pp
+++ modules/portage/manifests/init.pp
@@ -54,10 +54,12 @@ class portage {
     exec { "$name-use":
       command => "/usr/bin/perl -p -i -e 's/^.*$category.$name.*//g' \
                       /etc/portage/package.use && \
-                  /bin/echo '$category/$name $use' >>/etc/portage/package.use && \
+                  /bin/echo '$category/$name $use' \
+                      >>/etc/portage/package.use && \
                   /bin/sed '/^$/d' /etc/portage/package.use | \
                       /usr/bin/sort >/etc/portage/package.use.tmp && \
-                  /bin/mv /etc/portage/package.use.tmp /etc/portage/package.use",
+                  /bin/mv /etc/portage/package.use.tmp \
+                      /etc/portage/package.use",
       unless => "/bin/grep '$category/$name $use' /etc/portage/package.use \
                      1>/dev/null",
       before => Package[$name]
